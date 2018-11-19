b10cfed4004b974bb976ef276c8f4900d27a4214
itcast <bos@itcast.cn>
2018-11-19 16:40:56 +0800

    xxxx

---
+++ Git_demo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,22 @@
+package com.itheima.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
